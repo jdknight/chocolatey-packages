232e8b7d302b3ff49be4eebd4283c848662151aa
James Knight <james.d.knight@live.com>
2022-08-11 23:11:57 -0400

    replace source packaging with reference to sources
    It has been recommended to remove the inclusion of the source from the
    produced package. Sources were added to fulfill a license condition for
    the CVS application. However, we can alternatively just reference these
    sources instead. This commit drops the fetching/packaging of the
    source package and introduced a README file describing where sources can
    be acquired instead.
    
    Signed-off-by: James Knight <james.d.knight@live.com>

--- cvs/fetch.sh
+++ cvs/fetch.sh
@@ -4,7 +4,6 @@ version=$(grep -oP '(?<=<version>).*?(?=</version>)' cvs.nuspec)
 
 base_url=https://ftp.gnu.org/non-gnu/cvs
 binary_base_url=$base_url/binary/stable/x86-woe
-source_base_url=$base_url/source/stable
 
 bin_version=${version//./-}
 script_dir=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
@@ -14,15 +13,11 @@ work_dir=$script_dir/workdir
 artifacts=(
     $binary_base_url/cvs-${bin_version}.zip
     $binary_base_url/cvs-${bin_version}.zip.sig
-    $source_base_url/$version/cvs-$version.zip
-    $source_base_url/$version/cvs-$version.zip.sig
 )
 
 hashes=(
     635828e428d8f57e239bda9eadc05a26f12634da83c5d8453ad5f90f5bd2f418
     1b2f60c9bda3bbc9dd5933818b5138d416d0de023019f1096db33f51a984c681
-    aee0a02c31b7333217eefeb04c20b5e978d644cc4552f7e88a75a2871c719974
-    c5d2bbec33cd2c8a67c772c3254262cf8ceae9d397a82cdeb7e93cc256176476
 )
 
 mkdir -p $work_dir
@@ -62,7 +57,3 @@ done
 # extract the cvs executable
 echo "Extracting executable..."
 unzip -p $work_dir/cvs-${bin_version}.zip cvs.exe >$tools_dir/cvs.exe
-
-# copying source package (required for GPLv1)
-echo "Copying source package..."
-cp $work_dir/cvs-${version}.zip $tools_dir/
---
+++ cvs/legal/README.txt
@@ -0,0 +1,7 @@
+﻿CVS is licensed under GNU GENERAL PUBLIC LICENSE V1.
+
+See the full license text inside "LICENSE.txt".
+
+Sources for this program can be found at the following URL
+
+    https://ftp.gnu.org/non-gnu/cvs/
